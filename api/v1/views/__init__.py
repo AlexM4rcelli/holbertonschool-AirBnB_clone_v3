24136ec79539064b49747fc59134a9e60b259fd2
seba17962 <isebsebastiansilva@gmail.com>
2024-01-05 17:55:24 +0000

    first line

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 from flask import Blueprint
 from . import index, states, cities, amenities, users, places, reviews
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
